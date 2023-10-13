d5c7f82f7bedca2950dc40e6d8a85696e37bdf9c
akashiterable <akash.kumar@iterable.com>
2023-10-13 18:04:45 +0530

    test

---
+++ main.go
@@ -0,0 +1,11 @@
+package test
+
+import "fmt"
+
+func sayHello(name string) string {
+	if len(name) == 0 {
+		return "Hello Anonymous"
+	}
+
+	return fmt.Sprintf("Hello %s", name)
+}
---
+++ main_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"testing"
+)
+
+func Test_SayHello_ValidArgument(t *testing.T) {
+	inputs := []struct {
+		name   string
+		result string
+	}{
+		{name: "Yemeksepeti", result: "Hello Yemeksepeti"},
+		{name: "Banabi", result: "Hello Banabi"},
+		{name: "Yemek", result: "Hello Yemek"},
+	}
+
+	for _, item := range inputs {
+
+		result := sayHello(item.name)
+		if result != item.result {
+			t.Errorf("\"sayHello('%s')\" failed, expected -> %v, got -> %v", item.name, item.result, result)
+		} else {
+			t.Logf("\"sayHello('%s')\" succeded, expected -> %v, got -> %v", item.name, item.result, result)
+		}
+	}
+}
